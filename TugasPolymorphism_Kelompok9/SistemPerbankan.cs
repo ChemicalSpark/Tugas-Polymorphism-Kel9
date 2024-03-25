9e74536b221c939255e300b9bc13b225174636e8
degeneratezan <noxindocraft@gmail.com>
2024-03-25 10:47:07 +0700

    mengupdate perbankan

--- TugasPolymorphism_Kelompok9/SistemPerbankan.cs
+++ TugasPolymorphism_Kelompok9/SistemPerbankan.cs
@@ -1,51 +1,112 @@
-﻿//namespace Bank
-//{
-//    public class Rekening
-//    {
-//        protected double saldo;
+﻿namespace Bank
+{
+    public abstract class Rekening
+    {
+        public string Pemilik;
+        public double Saldo;
 
-//        public Rekening(double saldo)
-//        {
-//            this.saldo = saldo;
-//        }
+        public Rekening(string pemilik, double saldo)
+        {
+            Pemilik = pemilik;
+            Saldo = saldo;
+        }
 
-//        public abstract void Penarikan(double jumlah);
-//        public abstract void Penyetoran(double jumlah);
-//        public abstract void CekSaldo();
-//    }
+        public virtual void Setor(double jumlah)
+        {
+            Saldo += jumlah;
+        }
 
-//    public class RekeningTabungan : Rekening
-//    {
-//        public double bunga;
+        public virtual bool Tarik(double jumlah)
+        {
+            if (jumlah <= Saldo)
+            {
+                Saldo -= jumlah;
+                return true;
+            }
+            return false;
+        }
 
-//        public RekeningTabungan(double bunga,double saldo) : base(saldo)
-//        {
-//            this.bunga = bunga;
-//        }
+        public virtual bool Transfer(Rekening tujuan, double jumlah)
+        {
+            if (jumlah <= Saldo)
+            {
+                Saldo -= jumlah;
+                tujuan.Setor(jumlah);
+                return true;
+            }
+            return false;
+        }
 
-//        public override void Penarikan(double jumlah)
-//        {
-           
-//        }
+        public virtual double CekSaldo()
+        {
+            return Saldo;
+        }
+    }
 
-//        public override void Penyetoran(double jumlah)
-//        {
+    public class RekeningTabungan : Rekening
+    {
+        public double Bunga;
 
-//        }
+        public RekeningTabungan(string pemilik, double saldo, double bunga)
+            : base(pemilik, saldo)
+        {
+            Bunga = bunga;
+        }
 
-//        public override void CekSaldo()
-//        {
-            
-//        }
+        public override void Setor(double jumlah)
+        {
+            base.Setor(jumlah);
+            BerikanBunga();
+        }
 
-//        public void Bunga()
-//        {
+        private void BerikanBunga()
+        {
+            double bungaTahunan = Saldo * (Bunga / 100);
+            Saldo += bungaTahunan;
+        }
 
-//        }
-//    }
+        public string InfoTabungan()
+        {
+            return $"Info Rekening Tabungan {Pemilik} - Saldo: {Saldo} - Bunga: {Bunga}%";
+        }
+    }
+    public class RekeningGiro : Rekening
+    {
+        public double Bunga;
+        public DateTime JangkaWaktu;
 
-//    public class RekeningGiro : Rekening
-//    {
+        public RekeningGiro(string pemilik, double saldo, double bunga, DateTime jangkaWaktu)
+            : base(pemilik, saldo)
+        {
+            Bunga = bunga;
+            JangkaWaktu = jangkaWaktu;
+        }
 
-//    }
-//}
+        public override void Setor(double jumlah)
+        {
+            base.Setor(jumlah);
+            BerikanBunga();
+        }
+
+        private void BerikanBunga()
+        {
+            double bungaBulanan = Saldo * (Bunga / 100 / 12);
+            Saldo += bungaBulanan;
+        }
+
+        public override bool Tarik(double jumlah)
+        {
+
+            if (DateTime.Now <= JangkaWaktu)
+            {
+                return base.Tarik(jumlah);
+            }
+            return false;
+        }
+
+        public string InfoGiro()
+        {
+            return $"Info Rekening Giro {Pemilik} - Saldo: {Saldo} - Bunga: {Bunga}% - Jangka Waktu sampai : {JangkaWaktu} tahun";
+        }
+    }
+}
