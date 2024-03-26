e07a50c7206f1bcb8c693b2b269a713b14a2dea0
ChemicalSpark <rangga.pramudya23@gmail.com>
2024-03-26 11:52:36 +0700

    olshop fix final

--- TugasPolymorphism_Kelompok9/Program.cs
+++ TugasPolymorphism_Kelompok9/Program.cs
@@ -6,24 +6,42 @@ class Program
     static void Main(string[] args)
     {
         //Online Shop
-        KeranjangBelanja keranjang = new KeranjangBelanja(3);
-        Elektronik laptop = new Elektronik(5, "Razer Blade 18", 95000000);
-        Pakaian baju = new Pakaian("T-Shirt Console.WriteLine", 50000);
-        Buku komik = new Buku("Monster", 150000);
+        Console.WriteLine("Masukkan Keterangan Elektronik:");
+        Console.Write("Nama Elektronik: ");
+        string namaElektronik = Console.ReadLine();
+        Console.Write("Harga Elektronik: ");
+        decimal hargaElektronik = decimal.Parse(Console.ReadLine());
+        Console.Write("Berat Elektronik (kg): ");
+        double beratElektronik = double.Parse(Console.ReadLine());
+        Console.WriteLine("\nMasukkan Keterangan Pakaian:");
+        Console.Write("Nama Pakaian: ");
+        string namaPakaian = Console.ReadLine();
+        Console.Write("Harga Pakaian: ");
+        decimal hargaPakaian = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("\nMasukkan Keterangan Buku:");
+        Console.Write("Judul Buku: ");
+        string namaBuku = Console.ReadLine();
+        Console.Write("Harga Buku: ");
+        decimal hargaBuku = decimal.Parse(Console.ReadLine());
 
-        Console.WriteLine("---Keranjang Belanja---");
-        Console.WriteLine($"Nama item : {laptop.nama}, Harga item : {laptop.harga:c}, Berat item : {laptop.berat} Kg");
-        Console.WriteLine($"Nama item : {baju.nama}, Harga item : {baju.harga:c}");
-        Console.WriteLine($"Nama item : {komik.nama}, Harga item : {komik.harga:c}");
-        keranjang.tambahItem(laptop);
-        keranjang.tambahItem(baju);
-        keranjang.tambahItem(komik);
+        KeranjangBelanja keranjang = new KeranjangBelanja(3);
+        Elektronik Elektronik = new Elektronik(beratElektronik,namaElektronik,hargaElektronik);
+        Pakaian pakaian = new Pakaian(namaPakaian,hargaPakaian);
+        Buku buku = new Buku(namaBuku,hargaBuku);
+
+        Console.WriteLine("\n---Keranjang Belanja---");
+        Console.WriteLine($"Nama item : {namaElektronik}, Harga item : {hargaElektronik:c}, Berat item : {beratElektronik} Kg");
+        Console.WriteLine($"Nama item : {namaPakaian}, Harga item : {hargaPakaian:c}");
+        Console.WriteLine($"Nama item : {namaBuku}, Harga item : {hargaBuku:c}");
+        keranjang.tambahItem(Elektronik);
+        keranjang.tambahItem(pakaian);
+        keranjang.tambahItem(buku);
         decimal total = keranjang.hitungTotal();
         Console.WriteLine($"Total harga pada keranjang belanja : {+ total:c}");
         Console.WriteLine("\n---Ongkos Kirim---");
-        Console.WriteLine($"Ongkir untuk {laptop.nama} : {laptop.Ongkir():c}");
-        Console.WriteLine($"Ongkir untuk {baju.nama} : {baju.Ongkir():c}");
-        Console.WriteLine($"Ongkir untuk {komik.nama} : {komik.Ongkir():c}");
+        Console.WriteLine($"Ongkir untuk {namaElektronik} : {Elektronik.Ongkir():c}");
+        Console.WriteLine($"Ongkir untuk {namaPakaian} : {pakaian.Ongkir():c}");
+        Console.WriteLine($"Ongkir untuk {namaBuku} : {buku.Ongkir():c}");
         decimal totalOngkir = keranjang.hitungTotalOngkir();
         Console.WriteLine($"Total ongkir pada keranjang belanja : {+ totalOngkir:c}");
 
