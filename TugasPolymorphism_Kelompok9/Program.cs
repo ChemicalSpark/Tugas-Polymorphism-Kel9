812fe27f055d968443b444f65ab21cd5c340130f
ChemicalSpark <rangga.pramudya23@gmail.com>
2024-03-26 19:47:54 +0700

    perubahan terakhir fix

--- TugasPolymorphism_Kelompok9/Program.cs
+++ TugasPolymorphism_Kelompok9/Program.cs
@@ -9,54 +9,47 @@ class Program
     static void Main(string[] args)
     {
         ////Online Shop
-        //Console.WriteLine("Masukkan Keterangan Elektronik:");
-        //Console.Write("Nama Elektronik: ");
-        //string namaElektronik = Console.ReadLine();
-        //Console.Write("Harga Elektronik: ");
-        //decimal hargaElektronik = decimal.Parse(Console.ReadLine());
-        //Console.Write("Berat Elektronik (kg): ");
-        //double beratElektronik = double.Parse(Console.ReadLine());
-        //Console.WriteLine("\nMasukkan Keterangan Pakaian:");
-        //Console.Write("Nama Pakaian: ");
-        //string namaPakaian = Console.ReadLine();
-        //Console.Write("Harga Pakaian: ");
-        //decimal hargaPakaian = decimal.Parse(Console.ReadLine());
-        //Console.WriteLine("\nMasukkan Keterangan Buku:");
-        //Console.Write("Judul Buku: ");
-        //string namaBuku = Console.ReadLine();
-        //Console.Write("Harga Buku: ");
-        //decimal hargaBuku = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Masukkan Keterangan Elektronik:");
+        Console.Write("Nama Elektronik: ");
+        string namaElektronik = Console.ReadLine();
+        Console.Write("Harga Elektronik: ");
+        decimal hargaElektronik = decimal.Parse(Console.ReadLine());
+        Console.Write("Berat Elektronik (kg): ");
+        double beratElektronik = double.Parse(Console.ReadLine());
+        Console.WriteLine("\nMasukkan Keterangan Pakaian:");
+        Console.Write("Nama Pakaian: ");
+        string namaPakaian = Console.ReadLine();
+        Console.Write("Harga Pakaian: ");
+        decimal hargaPakaian = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("\nMasukkan Keterangan Buku:");
+        Console.Write("Judul Buku: ");
+        string namaBuku = Console.ReadLine();
+        Console.Write("Harga Buku: ");
+        decimal hargaBuku = decimal.Parse(Console.ReadLine());
 
-        //KeranjangBelanja keranjang = new KeranjangBelanja(3);
-        //Elektronik Elektronik = new Elektronik(beratElektronik,namaElektronik,hargaElektronik);
-        //Pakaian pakaian = new Pakaian(namaPakaian,hargaPakaian);
-        //Buku buku = new Buku(namaBuku,hargaBuku);
+        KeranjangBelanja keranjang = new KeranjangBelanja(3);
+        Elektronik Elektronik = new Elektronik(beratElektronik, namaElektronik, hargaElektronik);
+        Pakaian pakaian = new Pakaian(namaPakaian, hargaPakaian);
+        Buku buku = new Buku(namaBuku, hargaBuku);
 
-        //Console.WriteLine("\n---Keranjang Belanja---");
-        //Console.WriteLine($"Nama item : {namaElektronik}, Harga item : {hargaElektronik:c}, Berat item : {beratElektronik} Kg");
-        //Console.WriteLine($"Nama item : {namaPakaian}, Harga item : {hargaPakaian:c}");
-        //Console.WriteLine($"Nama item : {namaBuku}, Harga item : {hargaBuku:c}");
-        //keranjang.tambahItem(Elektronik);
-        //keranjang.tambahItem(pakaian);
-        //keranjang.tambahItem(buku);
-        //decimal total = keranjang.hitungTotal();
-        //Console.WriteLine($"Total harga pada keranjang belanja : {+ total:c}");
-        //Console.WriteLine("\n---Ongkos Kirim---");
-        //Console.WriteLine($"Ongkir untuk {namaElektronik} : {Elektronik.Ongkir():c}");
-        //Console.WriteLine($"Ongkir untuk {namaPakaian} : {pakaian.Ongkir():c}");
-        //Console.WriteLine($"Ongkir untuk {namaBuku} : {buku.Ongkir():c}");
-        //decimal totalOngkir = keranjang.hitungTotalOngkir();
-        //Console.WriteLine($"Total ongkir pada keranjang belanja : {+ totalOngkir:c}");
+        Console.WriteLine("\n---Keranjang Belanja---");
+        Console.WriteLine($"Nama item : {namaElektronik}, Harga item : {hargaElektronik:c}, Berat item : {beratElektronik} Kg");
+        Console.WriteLine($"Nama item : {namaPakaian}, Harga item : {hargaPakaian:c}");
+        Console.WriteLine($"Nama item : {namaBuku}, Harga item : {hargaBuku:c}");
+        keranjang.tambahItem(Elektronik);
+        keranjang.tambahItem(pakaian);
+        keranjang.tambahItem(buku);
+        decimal total = keranjang.hitungTotal();
+        Console.WriteLine($"Total harga pada keranjang belanja : {+total:c}");
+        Console.WriteLine("\n---Ongkos Kirim---");
+        Console.WriteLine($"Ongkir untuk {namaElektronik} : {Elektronik.Ongkir():c}");
+        Console.WriteLine($"Ongkir untuk {namaPakaian} : {pakaian.Ongkir():c}");
+        Console.WriteLine($"Ongkir untuk {namaBuku} : {buku.Ongkir():c}");
+        decimal totalOngkir = keranjang.hitungTotalOngkir();
+        Console.WriteLine($"Total ongkir pada keranjang belanja : {+totalOngkir:c}");
 
 
-<<<<<<< HEAD
         //Sistem Perbankan        
-=======
-        //Sistem Perbankan
-
-
-        
->>>>>>> d6325e867e0bce2eb85aa26b238b5ed3db6070c6
         Console.WriteLine("\n == Sistem Perbankan == \n");
 
 
@@ -92,7 +85,6 @@ class Program
 
         // Cek saldo setelah penarikan
         giroAni.CekSaldo();
-        
 
 
         // Penarikan melebihi saldo dan batas penarikan
@@ -110,6 +102,7 @@ class Program
 
 
         //Aplikasi Menggambar
+        Console.WriteLine("\n---Aplikasi Menggambar---");
         Canvas canvas = new Canvas();
 
         canvas.CreateShape(new SegiTiga("sama kaki", "merah") { JenisSegiTiga = "sama kaki", Warna = "Merah" });
